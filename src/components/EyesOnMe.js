9114ea536024cd75efbca90447be873f38379639
Mulu <brianmulu007@gmail.com>
2022-09-20 19:16:33 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe () {
+    const handleFocus = () => {console.log("Good!")}
+    const handleBlur = () => {console.log("Hey! Eyes on me!")}
+    return (<button Focus = {handleFocus} Blur = {handleBlur} > Eyes on me </button>)
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event) {
+        console.log(`${event.target.placeholder}`)
+    }
     return (
-        <div></div>
+        <div>
+            <input
+            type = "password"
+            onChange={handleChange}
+            placeholder = "Entering password..."
+            />
+        </div>
     )
 }
 
